4d4b3ddc515d484bc0a07c818a6394484d0a9ac3
kornman00 <kornman00@gmail.com>
2015-01-08 12:46:47 -0600

    * KSoft.T4: Some codegen util for generating vector types

---
+++ KSoft.T4/Math/MathT4.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using TextTemplating = Microsoft.VisualStudio.TextTemplating;
+
+namespace KSoft.T4.Math
+{
+	public static class MathT4
+	{
+		public struct VectorComponent
+		{
+			public readonly string Name;
+			public readonly int Index;
+			public readonly bool LastComponent;
+
+			internal VectorComponent(int index, string name, int vecDimensions = 0)
+			{
+				Index = index;
+				Name = name;
+				LastComponent = index == (vecDimensions-1);
+			}
+
+			public string Prefix(string prefix)
+			{
+				return prefix + Name;
+			}
+			public string Suffix(string suffix)
+			{
+				return Name + suffix;
+			}
+
+			public string ContOrEnd(string cont, string end = "")
+			{
+				return LastComponent
+					? end
+					: cont;
+			}
+		};
+
+		public class VectorDef
+		{
+			public NumberCodeDefinition CodeDef;
+			public int Dimensions;
+
+			public VectorDef(NumberCodeDefinition codeDef, int dimensions)
+			{
+				CodeDef = codeDef;
+				Dimensions = dimensions;
+			}
+
+			public string TypeName { get {
+				string typeChar = CodeDef.IsInteger
+					? "i"
+					: "f";
+
+				return string.Format("Vec{0}{1}{2}",
+					Dimensions, typeChar, CodeDef.SizeOfInBits);
+			} }
+
+			public IEnumerable<VectorComponent> Components { get {
+				if (Dimensions >= 1) yield return new VectorComponent(0, "x", Dimensions);
+				if (Dimensions >= 2) yield return new VectorComponent(1, "y", Dimensions);
+				if (Dimensions >= 3) yield return new VectorComponent(2, "z", Dimensions);
+			} }
+
+			public bool ComponentsRequireCast { get { return CodeDef.SizeOfInBytes < PrimitiveDefinitions.kInt32.SizeOfInBytes; } }
+
+			public string ComponentDecls(string prefix = "", string suffix = "")
+			{
+				var sb = new System.Text.StringBuilder();
+
+				sb.Append(CodeDef.Keyword);
+				sb.Append(" ");
+
+				foreach (var comp in Components)
+				{
+					sb.Append(prefix);
+					sb.Append(comp.Name);
+					sb.Append(suffix);
+
+					if (!comp.LastComponent)
+						sb.Append(", ");
+				}
+
+				return sb.ToString();
+			}
+
+			public string ComponentParams(string prefix = "", string suffix = "")
+			{
+				var sb = new System.Text.StringBuilder();
+
+				foreach (var comp in Components)
+				{
+					sb.Append(CodeDef.Keyword);
+					sb.Append(" ");
+
+					sb.Append(prefix);
+					sb.Append(comp.Name);
+					sb.Append(suffix);
+
+					if (!comp.LastComponent)
+						sb.Append(", ");
+				}
+
+				return sb.ToString();
+			}
+
+			public string ComponentArgs(string prefix = "", string suffix = "", bool useCastsIfNeeded = false)
+			{
+				var sb = new System.Text.StringBuilder();
+
+				if (useCastsIfNeeded)
+					useCastsIfNeeded = ComponentsRequireCast;
+
+				foreach (var comp in Components)
+				{
+					if (useCastsIfNeeded)
+						sb.AppendFormat("({0})( ", CodeDef.Keyword);
+
+					sb.Append(prefix);
+					sb.Append(comp.Name);
+					sb.Append(suffix);
+
+					if (useCastsIfNeeded)
+						sb.Append(" )");
+
+					if (!comp.LastComponent)
+						sb.Append(", ");
+				}
+
+				return sb.ToString();
+			}
+		};
+		public static IEnumerable<VectorDef> IntegralVectors { get {
+			yield return new VectorDef(PrimitiveDefinitions.kInt16, 2);
+			yield return new VectorDef(PrimitiveDefinitions.kInt16, 3);
+			yield return new VectorDef(PrimitiveDefinitions.kInt32, 2);
+			yield return new VectorDef(PrimitiveDefinitions.kInt32, 3);
+		} }
+	};
+}
