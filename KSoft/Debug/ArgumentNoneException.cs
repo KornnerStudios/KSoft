785d3d19f46c9d849a093cb4aa1ad938dd2b2a57
kornman00 <kornman00@gmail.com>
2019-12-08 21:12:13 -0800

    Add ArgumentNoneException.

---
+++ KSoft/Debug/ArgumentNoneException.cs
@@ -0,0 +1,17 @@
+﻿
+namespace KSoft
+{
+	/// <summary>Exception thrown when a handle value is NONE where it is not a supported case</summary>
+	public class ArgumentNoneException : System.ArgumentException
+	{
+		public ArgumentNoneException() : base()
+		{
+		}
+
+		public ArgumentNoneException(string paramName) : base("Parameter cannot be NONE", paramName) {}
+		public ArgumentNoneException(string paramName, System.Exception innerException) : base("Parameter cannot be NONE", paramName, innerException) {}
+
+		public ArgumentNoneException(string msg, string paramName) : base(msg, paramName) {}
+		public ArgumentNoneException(string msg, string paramName, System.Exception innerException) : base(msg, paramName, innerException) {}
+	};
+}
