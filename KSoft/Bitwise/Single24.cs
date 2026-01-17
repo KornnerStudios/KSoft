39528dab513238b6ce99474f82768d35ce132726
kornman00 <kornman00@gmail.com>
2026-01-17 10:56:26 -0800

    Update Single24 utils
    Replace non-exhaustive FromSingle method with a proper TryFromSingle.
    Update Single24Test code to include all known 24-bit values from Halo Wars Definitive Edition.

--- KSoft/Bitwise/Single24.cs
+++ KSoft/Bitwise/Single24.cs
@@ -1,61 +1,96 @@
 ﻿using System;
+#if CONTRACTS_FULL_SHIM
+using Contract = System.Diagnostics.ContractsShim.Contract;
+#else
+using Contract = System.Diagnostics.Contracts.Contract; // SHIM'D
+#endif
 
 namespace KSoft.Bitwise
 {
+	/// <summary>
+	/// Utilities for converting between 32-bit and 24-bit single-precision floating point values.
+	/// </summary>
+	/// <remarks>
+	/// 24-bit: s1e6m17, meaning: 1 sign bit, 6 exponent bits, 17 mantissa bits
+	/// 32-bit: s1e8m23, meaning: 1 sign bit, 8 exponent bits, 23 mantissa bits
+	/// </remarks>
 	public static class Single24
 	{
 		public const float MinValue = kMin;
 		public const float MaxValue = kMax;
 
+		// s1e8m23
 		#region Single bit definitions
 		static class Single32
 		{
 			internal const int kMantissaBitIndex = 0;
 			internal const int kMantissaBitCount = 23;
-			// 0x007FFFFF, (1 << 23) - 1
+			/// <summary>0x007FFFFF = (1 LHS 23) - 1</summary>
 			internal const uint kMantissaBitMask = (1U<<kMantissaBitCount)-1;
 
+			/// <summary>23</summary>
 			internal const int kExponentBitIndex = kMantissaBitIndex + kMantissaBitCount;
 			internal const int kExponentBitCount = 8;
-			// 0x7F800000, ((1 << 8) - 1) << 23
+			/// <summary>0x7F800000 = ((1 LHS 8) - 1) LHS 23</summary>
 			internal const uint kExponentBitMask = ((1U<<kExponentBitCount)-1) << kExponentBitIndex;
 
+			/// <summary>31</summary>
 			internal const int kSignBitIndex = kExponentBitIndex + kExponentBitCount;
 			internal const int kSignBitCount = 1;
-			// 0x80000000, 1 << 31
+			/// <summary>0x80000000 = 1 LHS 31</summary>
 			internal const uint kSignBitMask = 1U << kSignBitIndex;
 
+			/// <summary>0x80000000</summary>
 			internal const uint kSignBit = kSignBitMask;
 
+			/// <summary>127</summary>
 			internal const int kExponentBias = (1 << (kExponentBitCount-1)) - 1;
 		};
 		#endregion
 
+		// s1e6m17
 		#region Single24 bit definitions
 		const int kMantissaBitIndex = 0;
 		const int kMantissaBitCount = 17;
-		// 0x0001FFFF, (1 << 17) - 1
+		/// <summary>0x0001FFFF = (1 LHS 17) - 1</summary>
 		const uint kMantissaBitMask = (1U << kMantissaBitCount) - 1;
 
+		/// <summary>17</summary>
 		const int kExponentBitIndex = kMantissaBitIndex + kMantissaBitCount;
 		const int kExponentBitCount = 6;
-		// 0x007E0000, ((1 << 6) - 1) << 17
+		/// <summary>63 = (1 LHS 6) - 1</summary>
+		const uint kExponentMaxValue = (1U << kExponentBitCount) - 1;
+		/// <summary>32 = (1 LHS 6) RHS 1</summary>
+		const uint kExponentMidpoint = (1U << kExponentBitCount) >> 1;
+		/// <summary>0x007E0000 = ((1 LHS 6) - 1) LHS 17</summary>
 		const uint kExponentBitMask = ((1U << kExponentBitCount) - 1) << kExponentBitIndex;
 
+		/// <summary>23</summary>
 		const int kSignBitIndex = kExponentBitIndex + kExponentBitCount;
 		const int kSignBitCount = 1;
-		// 0x00800000, 1 << 23
+		/// <summary>0x00800000 = 1 LHS 23</summary>
 		const uint kSignBitMask = 1U << kSignBitIndex;
 
+		/// <summary>0x00800000</summary>
 		const uint kSignBit = kSignBitMask;
 
+		/// <summary>31</summary>
 		const int kExponentBias = (1 << (kExponentBitCount - 1)) - 1;
+		/// <summary>96 = 127 - 31</summary>
 		const int kExponentBiasDiff = Single32.kExponentBias - Single24.kExponentBias;
 
+		/// <summary>6 = 23 - 17</summary>
 		const int kMantissaBitDiff = Single32.kMantissaBitCount - kMantissaBitCount;
+		/// <summary>64 = 1 LHS 6</summary>
+		const uint kMantissaBitDiffMaxValue = 1U << kMantissaBitDiff;
+		/// <summary>63 = (1 LHS 6) - 1</summary>
+		const uint kMantissaBitDiffBitMask = (1U << kMantissaBitDiff) - 1;
 		/// <remarks>Exponent is encoded in offset-binary, so we can't just shift the bits like mantissa</remarks>
 		[Obsolete]
 		const int kExponentBitDiff = Single32.kExponentBitCount - kExponentBitCount;
+
+		/// <summary>0xFFFFFF = 0x00800000 | 0x007E0000 | 0x0001FFFF</summary>
+		const uint kBitMask = kSignBitMask | kExponentBitMask | kMantissaBitMask;
 		#endregion
 
 		#region Min\Max
@@ -66,11 +101,103 @@ namespace KSoft.Bitwise
 		const float kMax = 8.589902E+09F;
 		#endregion
 
+		/// <summary>
+		/// Is the given 32-bit float value in range of what Single24 can roughly represent?
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
 		public static bool InRange(float value)
+			=> value >= kMin && value <= kMax;
+
+		public static bool TryFromSingle(float singleValue, out uint encodedBits)
 		{
-			return value >= kMin && value <= kMax;
+			encodedBits = 0;
+
+			uint singleValueAsUInt = Bitwise.ByteSwap.SingleToUInt32(singleValue);
+			uint mantissa = (singleValueAsUInt & Single32.kMantissaBitMask) >> Single32.kMantissaBitIndex;
+			uint exponentBits = (singleValueAsUInt & Single32.kExponentBitMask) >> Single32.kExponentBitIndex;
+			uint sign = (singleValueAsUInt & Single32.kSignBitMask) >> Single32.kSignBitIndex;
+
+			// handle zero / denormalized values
+			if (exponentBits == 0)
+			{
+				encodedBits = sign == 1
+					? kSignBit // -0.0
+					: 0; // 0.0
+				return true;
+			}
+
+			// first get the true Single32 exponent
+			uint exponent = exponentBits - Single32.kExponentBias;
+			// then put the exponent into the Single24 format
+			uint newExponent = exponent + Single24.kExponentBias;
+			// it should not actually be possible to get here with an exponent of 0
+			// as we already handled that case above, checking exponentBits
+			if (newExponent == 0 || newExponent > Single24.kExponentMaxValue)
+			{
+				return false;
+			}
+
+			// the fractional remainder of the 6 bits which are going to get discarded
+			uint remainder = mantissa & Single24.kMantissaBitDiffBitMask;
+			// clear bottom 5 bits of the original 17-bit mantissa
+			// to get the initial rounded-down value
+			uint newMantissa = mantissa & ~Single24.kMantissaBitDiffBitMask;
+
+			// round-to-nearest-even ("banker's rounding") to match IEEE 754
+			if (remainder >= Single24.kExponentMidpoint)
+			{
+				bool roundUp = false;
+				// check for tie-breaker
+				if (remainder == Single24.kExponentMidpoint)
+				{
+					// do we need to round toward even?
+					if ((newMantissa & kMantissaBitDiffMaxValue) != 0)
+					{
+						roundUp = true;
+					}
+				}
+				else
+				{
+					roundUp = false;
+				}
+
+				if (roundUp)
+				{
+					// same as doing += kMantissaBitDiffMaxValue
+					newMantissa |= (1U << kMantissaBitDiff);
+					newMantissa += kMantissaBitDiffMaxValue;
+					// check for overflow
+					if (newMantissa >= Single32.kMantissaBitMask)
+					{
+						newMantissa = 0;
+						newExponent++;
+						if (newExponent > Single24.kExponentMaxValue)
+						{
+							return false;
+						}
+					}
+				}
+			}
+			else
+			{
+				// keep newMantissa as-is (round down)
+			}
+
+			newMantissa >>= kMantissaBitDiff;
+
+			Contract.Assert(newExponent>=1 && newExponent<=Single24.kExponentMaxValue);
+			Contract.Assert(newMantissa <= Single24.kMantissaBitMask);
+			uint v = newMantissa;
+			v |= newExponent << Single24.kExponentBitIndex;
+			v |= sign == 1 ? Single24.kSignBit : 0U;
+			Contract.Assert(v <= Single24.kBitMask);
+
+			encodedBits = v;
+			return true;
 		}
 
+		[System.Obsolete($"Use {nameof(TryFromSingle)}")]
 		public static uint FromSingle(float singleValue)
 		{
 			uint data = Bitwise.ByteSwap.SingleToUInt32(singleValue);
@@ -102,22 +229,33 @@ namespace KSoft.Bitwise
 		public static float ToSingle(uint data)
 		{
 			uint mantissa = (data & kMantissaBitMask) >> kMantissaBitIndex;
-			uint exponent = (data & kExponentBitMask) >> kExponentBitIndex;
+			uint exponentBits = (data & kExponentBitMask) >> kExponentBitIndex;
 			uint sign = (data & kSignBitMask) >> kSignBitIndex;
 			uint v;
 
-			if (exponent == 0)
+			if (exponentBits == 0)
 			{
-				v = Single32.kSignBit;
+				v = sign == 1
+					? Single32.kSignBit // -0.0
+					: 0;
 			}
 			else
 			{
-				sign = sign == 1 ? Single32.kSignBit : 0;
-				exponent += kExponentBiasDiff;
-				exponent <<= kExponentBitIndex;
+				sign = sign == 1
+					? Single32.kSignBit
+					: 0;
 
-				v = exponent | mantissa;
-				v <<= kMantissaBitDiff;
+				// We used to have this code, but it isn't as clear what is taking place
+				// so break it into exponent and newExponent
+				//exponentBits += Single24.kExponentBiasDiff;
+				uint exponent = exponentBits - Single24.kExponentBias;
+				uint newExponent = exponent + Single32.kExponentBias;
+
+				// lowest bits are the mantissa
+				// account for the larger amount of mantissa bits in Single32 vs 24
+				v  = mantissa << Single24.kMantissaBitDiff;
+				// shift over the exponent after the preceding mantissa bits
+				v |= newExponent << Single32.kMantissaBitCount;
 				v |= sign;
 			}
 
--- Test.KSoft.BCL/Bitwise/Single24Test.cs
+++ Test.KSoft.BCL/Bitwise/Single24Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KSoft.Bitwise.Test
@@ -24,11 +25,134 @@ namespace KSoft.Bitwise.Test
 				uint expected = kTestConversionsList[x + 1];
 
 				float single = Single24.ToSingle(input);
-				uint output = Single24.FromSingle(single);
+				bool was_encoded = Single24.TryFromSingle(single, out uint output);
 
+				Assert.IsTrue(was_encoded);
 				Assert.AreEqual(ByteSwap.SingleFromUInt32(expected), single); // Test ToSingle
 				Assert.AreEqual(input, output); // Test FromSingle
 			}
 		}
+
+		#region Halo Wars Definitive Edition tests
+		[TestMethod]
+		public void Single24_TestHaloWarsDefinitiveEditionValues()
+		{
+			foreach (var kvp in kHaloWarsDefinitiveEditionValues)
+			{
+				uint expectedSingle24Bits = kvp.Key;
+				float expectedFloat = kvp.Value;
+
+				float actualFloat = Single24.ToSingle(expectedSingle24Bits);
+				bool expectedFloatWasEncoded = Single24.TryFromSingle(expectedFloat, out uint actualSingle24Bits);
+
+				Assert.AreEqual(expectedFloat, actualFloat);
+				Assert.IsTrue(expectedFloatWasEncoded);
+				Assert.AreEqual(expectedSingle24Bits, actualSingle24Bits);
+			}
+		}
+
+		static readonly KeyValuePair<uint, float>[] kHaloWarsDefinitiveEditionValues =
+		[
+			new(0x000000, 0f),             // data\tactics\cov_air_banshee_01.tactics.xmb
+			new(0x1618DF, 1.0000003E-06f), // art\effects\cleansing\cleansing_air_impact_large_a.vis.xmb
+			new(0x30C5F9, 0.010833323f),   // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x341B4F, 0.032916784f),   // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x342222, 0.0333333f),     // art\effects\shield_dome\shield_flicker_animation_01.xml.xmb
+			new(0x35B281, 0.057770014f),   // art\ui\flash\hud\hud_minimap\hud_minimap.xml.xmb
+			new(0x3645A2, 0.0710001f),     // art\campaign\scn02\rocketwarthog_01\rocketwarthog_01.vis.xmb
+			new(0x39B3C4, 0.23138809f),    // art\ui\flash\hud\hud_minimap\hud_minimap.xml.xmb
+			new(0x3BF47C, 0.49437714f),    // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3D6222, 0.8458328f),     // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3D83D7, 0.87874985f),    // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3D9A74, 0.90083313f),    // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3DBC29, 0.93375015f),    // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3DDDDE, 0.9666672f),     // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3DFFC9, 0.9997902f),     // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3EAE30, 1.34021f),       // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x3F333F, 1.60009f),       // physics\cov_bldg_temple_02.shp.xmb
+			new(0x3F7777, 1.7333298f),     // art\effects\shield_dome\shield_flicker_animation_01.xml.xmb
+			new(0x3F8889, 1.7666702f),     // art\effects\shield_dome\shield_flicker_animation_01.xml.xmb
+			new(0x407921, 2.4731598f),     // physics\cov_bldg_temple_02.shp.xmb
+			new(0x44633F, 9.55072f),       // art\effects\shield_dome\shield_fade_fast_01.xml.xmb
+			new(0x515200, 850f),           // data\objects.xml.xmb
+			new(0x515400, 852f),           // data\objects.xml.xmb
+			new(0x51E800, 1000f),          // data\tactics\cov_air_vampire_01.tactics.xmb
+			new(0x521C00, 1080f),          // data\objects.xml.xmb
+			new(0x522600, 1100f),          // data\tactics\cpgn_npc_forgewarthog_01.tactics.xmb
+			new(0x522D00, 1114f),          // data\objects.xml.xmb
+			new(0x524E80, 1181f),          // data\objects.xml.xmb
+			new(0x525800, 1200f),          // data\objects.xml.xmb
+			new(0x527100, 1250f),          // data\tactics\for_air_enforcer_01.tactics.xmb
+			new(0x528000, 1280f),          // data\objects.xml.xmb
+			new(0x528600, 1292f),          // data\objects.xml.xmb
+			new(0x529A80, 1333f),          // data\objects.xml.xmb
+			new(0x52BC00, 1400f),          // data\tactics\cov_inf_suicidegrunt_01.tactics.xmb
+			new(0x52EA00, 1492f),          // data\objects.xml.xmb
+			new(0x52EE00, 1500f),          // data\tactics\cov_air_banshee_01.tactics.xmb
+			new(0x533980, 1651f),          // data\objects.xml.xmb
+			new(0x533B80, 1655f),          // data\tactics\pow_gp_rage_impact.tactics.xmb
+			new(0x536600, 1740f),          // data\objects.xml.xmb
+			new(0x53E800, 2000f),          // data\objects.xml.xmb
+			new(0x543A00, 2280f),          // data\objects.xml.xmb
+			new(0x5461C0, 2439f),          // data\objects.xml.xmb
+			new(0x547100, 2500f),          // data\aidifficultysettings.xml.xmb
+			new(0x54EE00, 3000f),          // data\tactics\cpgn_npc_forge_01.tactics.xmb
+			new(0x54FA40, 3049f),          // data\techs.xml.xmb
+			new(0x5518C0, 3171f),          // data\objects.xml.xmb
+			new(0x552000, 3200f),          // data\tactics\unsc_air_vulture_01.tactics.xmb
+			new(0x552E80, 3258f),          // data\objects.xml.xmb
+			new(0x556B00, 3500f),          // data\powers.xml.xmb
+			new(0x558400, 3600f),          // data\objects.xml.xmb
+			new(0x558E80, 3642f),          // data\objects.xml.xmb
+			new(0x55B300, 3788f),          // data\objects.xml.xmb
+			new(0x55CFC0, 3903f),          // data\objects.xml.xmb
+			new(0x55E700, 3996f),          // data\objects.xml.xmb
+			new(0x55E800, 4000f),          // data\objects.xml.xmb
+			new(0x561340, 4250f),          // data\objects.xml.xmb
+			new(0x5624E0, 4391f),          // data\objects.xml.xmb
+			new(0x567100, 5000f),          // data\tactics\cov_bldg_megaturret_01.tactics.xmb
+			new(0x5692A0, 5269f),          // data\objects.xml.xmb
+			new(0x56EE00, 6000f),          // data\objects.xml.xmb
+			new(0x56F1C0, 6030f),          // data\objects.xml.xmb
+			new(0x56FF20, 6137f),          // data\objects.xml.xmb
+			new(0x570940, 6218f),          // data\objects.xml.xmb
+			new(0x5711A0, 6285f),          // data\objects.xml.xmb
+			new(0x572000, 6400f),          // data\objects.xml.xmb
+			new(0x572C80, 6500f),          // data\tactics\cpgn_scn07_scarabboss_01.tactics.xmb
+			new(0x57A980, 7500f),          // data\objects.xml.xmb
+			new(0x57E800, 8000f),          // data\objects.xml.xmb
+			new(0x57EF20, 8057f),          // data\objects.xml.xmb
+			new(0x580830, 8323f),          // data\objects.xml.xmb
+			new(0x581660, 8550f),          // data\objects.xml.xmb
+			new(0x581C00, 8640f),          // data\objects.xml.xmb
+			new(0x5870F0, 9999f),          // data\aidifficultysettings.xml.xmb
+			new(0x587100, 10000f),         // data\aidifficultysettings.xml.xmb
+			new(0x589040, 10500f),         // data\objects.xml.xmb
+			new(0x58E1F0, 11807f),         // data\objects.xml.xmb
+			new(0x58EE00, 12000f),         // data\objects.xml.xmb
+			new(0x58FFC0, 12284f),         // data\objects.xml.xmb
+			new(0x596F60, 14070f),         // data\objects.xml.xmb
+			new(0x59A980, 15000f),         // data\objects.xml.xmb
+			new(0x5A1340, 17000f),         // data\objects.xml.xmb
+			new(0x5A3280, 18000f),         // data\tactics\pow_cp_largeexplode_01.tactics.xmb
+			new(0x5A7100, 20000f),         // data\objects.xml.xmb
+			new(0x5A8230, 20550f),         // data\objects.xml.xmb
+			new(0x5AEE00, 24000f),         // data\objects.xml.xmb
+			new(0x5B0D40, 25000f),         // data\objects.xml.xmb
+			new(0x5B3C20, 26500f),         // data\objects.xml.xmb
+			new(0x5BA980, 30000f),         // data\objects.xml.xmb
+			new(0x5BAB58, 30059f),         // data\objects.xml.xmb
+			new(0x5C3280, 36000f),         // data\objects.xml.xmb
+			new(0x5C7100, 40000f),         // data\tactics\cov_veh_brutechopper_01.tactics.xmb
+			new(0x5D0D40, 50000f),         // data\objects.xml.xmb
+			new(0x5D48F0, 53820f),         // data\objects.xml.xmb
+			new(0x5DA980, 60000f),         // data\tactics\cov_bldg_megaturret_01.tactics.xmb
+			new(0x5DB8A8, 60970f),         // data\objects.xml.xmb
+			new(0x5E22E0, 70000f),         // data\tactics\hook_bldg_megaturret_01.tactics.xmb
+			new(0x5E49F0, 75000f),         // data\objects.xml.xmb
+			new(0x5E7100, 80000f),         // data\objects.xml.xmb
+			new(0xDD0D40, -50000f),        // data\techs.xml.xmb
+		];
+		#endregion
 	};
 }
